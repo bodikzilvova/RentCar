8baadd89f1ea5d2741bd63ae1895ce895c39685a
bodikzilvova <103454649+bodikzilvova@users.noreply.github.com>
2024-01-29 20:03:55 +0000

    end-with-modal

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Accessories,
   Backdrop,
@@ -9,10 +9,10 @@ import {
   ColorText,
   ConditionWrapper,
   Conditions,
-  Description,
   FlexWrapper,
   Functionalities,
   Img,
+  PositionWrapper,
   RentBtn,
   RentalTitle,
   SpanLine,
@@ -20,82 +20,103 @@ import {
   Wrapper,
   WrapperInfo,
 } from './ModalWindow.styled';
+import { RxCross1 } from 'react-icons/rx';
 
 function ModalWindow({ item, closeModal }) {
-  console.log(item);
-
   const rentalConditions = item.rentalConditions.split('\n');
- const age = rentalConditions[0].split(':')[1].trim().split(' ')[0];
+  const age = rentalConditions[0].split(':')[1].trim().split(' ')[0];
 
-  
+  const handleClickOutside = event => {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.key === 'Escape') {
+        closeModal();
+      }
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [closeModal]);
+
+  const handleGoToGitHub = () => {
+    window.open('https://github.com/bodikzilvova', '_blank');
+  };
 
   return (
-    <Backdrop>
-      <Wrapper>
-        <CloseBtn onClick={closeModal}>X</CloseBtn>
+    <Backdrop onClick={handleClickOutside}>
+      <PositionWrapper>
+        <Wrapper>
+          <CloseBtn onClick={closeModal}>
+            <RxCross1
+              style={{ width: '12px', height: '12px', strokeWidth: '1px' }}
+            />
+          </CloseBtn>
 
-        <Img src={item.img} />
-        <WrapperInfo>
-          <CarName>{item.make}</CarName>
-          <CarModel>{item.model}</CarModel>,<CarYear>{item.year}</CarYear>
-        </WrapperInfo>
-
-        <TextWrapper>
-          {item.city}
-          <SpanLine />
-          {item.country}
-          <SpanLine />
-          Year: {item.year}
-          <SpanLine />
-          Type: {item.type}
-          <SpanLine />
-          Fuel Consumption: {item.fuelConsumption} <SpanLine /> Engine Size:
-          {item.engineSize}
-        </TextWrapper>
-
-        <Description>{item.description}</Description>
-
-        <Functionalities>
-          <Accessories>Accessories and functionalities:</Accessories>
+          <Img src={item.img} />
+          <WrapperInfo>
+            <CarName>{item.make}</CarName>
+            <CarModel>{item.model}</CarModel>,<CarYear>{item.year}</CarYear>
+          </WrapperInfo>
 
           <TextWrapper>
-            {item.accessories[0]}
+            {item.city}
             <SpanLine />
-            {item.accessories[1]}
+            {item.country}
             <SpanLine />
-            {item.accessories[2]}
+            Year: {item.year}
             <SpanLine />
-            {item.functionalities[0]}
+            Type: {item.type}
             <SpanLine />
-            {item.functionalities[1]}
-            <SpanLine />
-            {item.functionalities[2]}
+            Fuel Consumption: {item.fuelConsumption} <SpanLine /> Engine Size:
+            {item.engineSize}
           </TextWrapper>
-        </Functionalities>
 
-        <Conditions>
-          <RentalTitle>Rental Conditions:</RentalTitle>
-          <FlexWrapper>
-            <ConditionWrapper>
-              Mimimum age: <ColorText>{age}</ColorText>
-            </ConditionWrapper>
-            <ConditionWrapper>
-              {rentalConditions[1]}
-            </ConditionWrapper>
-            <ConditionWrapper>
-            {rentalConditions[2]}
-            </ConditionWrapper>
-            <ConditionWrapper>
-            Mileage: <ColorText>{item.mileage}</ColorText>
-            </ConditionWrapper>
-            <ConditionWrapper>
-              Price: <ColorText>{item.rentalPrice}</ColorText>
-            </ConditionWrapper>
-          </FlexWrapper>
-        </Conditions>
+          {/* <Description>{item.description}</Description> */}
 
-        <RentBtn>Rental Car</RentBtn>
-      </Wrapper>
+          <Functionalities>
+            <Accessories>Accessories and functionalities:</Accessories>
+
+            <TextWrapper>
+              {item.accessories[0]}
+              <SpanLine />
+              {item.accessories[1]}
+              <SpanLine />
+              {item.accessories[2]}
+              <SpanLine />
+              {item.functionalities[0]}
+              <SpanLine />
+              {item.functionalities[1]}
+              <SpanLine />
+              {item.functionalities[2]}
+            </TextWrapper>
+          </Functionalities>
+
+          <Conditions>
+            <RentalTitle>Rental Conditions:</RentalTitle>
+            <FlexWrapper>
+              <ConditionWrapper>
+                Mimimum age: <ColorText>{age}</ColorText>
+              </ConditionWrapper>
+              <ConditionWrapper>{rentalConditions[1]}</ConditionWrapper>
+              <ConditionWrapper>{rentalConditions[2]}</ConditionWrapper>
+              <ConditionWrapper>
+                Mileage: <ColorText>{item.mileage}</ColorText>
+              </ConditionWrapper>
+              <ConditionWrapper>
+                Price: <ColorText>{item.rentalPrice}</ColorText>
+              </ConditionWrapper>
+            </FlexWrapper>
+          </Conditions>
+
+          <RentBtn onClick={handleGoToGitHub}>Rental Car</RentBtn>
+        </Wrapper>
+      </PositionWrapper>
     </Backdrop>
   );
 }
--- src/components/ModalWindow/ModalWindow.styled.js
+++ src/components/ModalWindow/ModalWindow.styled.js
@@ -5,28 +5,54 @@ export const Backdrop = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  top: 0px;
-  left: 0px;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100vh;
   background: rgba(18, 20, 23, 0.5);
 `;
 
-export const Wrapper = styled.div`
-  max-width: 541px;
-  max-height: 100%;
+export const PositionWrapper = styled.div`
+  position: relative;
   border-radius: 24px;
-  border: none;
   background-color: #fff;
-  padding: 40px;
+`;
 
+export const Wrapper = styled.div`
+  width: 541px;
+  height: 100%;
+  padding: 40px;
 
   display: flex;
   flex-direction: column;
 `;
 
 export const CloseBtn = styled.button`
+width: 24px;
+height: 24px;
+position: absolute;
+top: 20px;
+left: 501px;
+background-color: inherit;
+border: none;
+cursor: pointer;
+display: flex;
+align-items: center;
+justify-content: center;
+outline: none;
 
+svg {
+    stroke: transparent;
+    transition: stroke 0.3s ease-in-out;
+  }
+
+  &:hover,
+  &:focus {
+    svg {
+      stroke: #3470ff;
+    }
+  }
+}
 `;
 
 export const Img = styled.img`
@@ -89,55 +115,56 @@ export const Functionalities = styled.div`
 `;
 
 export const Accessories = styled.h2`
-font-family: Manrope, sans-serif;
+  font-family: Manrope, sans-serif;
   font-weight: 500;
   font-size: 14px;
   line-height: 1.42;
   margin-bottom: 8px;
-`
+`;
 
 export const Conditions = styled.div``;
 
 export const RentalTitle = styled.h2`
-font-family: Manrope, sans-serif;
+  font-family: Manrope, sans-serif;
   font-weight: 500;
   font-size: 14px;
   line-height: 1.42;
   margin-bottom: 8px;
-`
+`;
 export const FlexWrapper = styled.div`
-display: flex;
-flex-wrap: wrap;
-gap: 8px;
-margin-bottom: 16px;
-`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  margin-bottom: 16px;
+`;
 
 export const ConditionWrapper = styled.div`
-background-color: #F9F9F9;
-height: 32px;
-border-radius: 32px;
-padding: 7px 14px;
-margin-bottom: 8px;
+  background-color: #f9f9f9;
+  height: 32px;
+  border-radius: 32px;
+  padding: 7px 14px;
+  margin-bottom: 8px;
 `;
 
 export const ColorText = styled.span`
-font-family: Manrope, sans-serif;
+  font-family: Manrope, sans-serif;
   font-weight: 400;
   font-size: 12px;
   line-height: 1.5;
-  color: #3470FF;
+  color: #3470ff;
 `;
 
 export const RentBtn = styled.button`
-width: 168px;
-height: 44px;
-border-radius: 12px;
-border: none;
-background-color: #3470FF;
+  width: 168px;
+  height: 44px;
+  border-radius: 12px;
+  border: none;
+  background-color: #3470ff;
+  cursor: pointer;
 
-font-family: Manrope, sans-serif;
+  font-family: Manrope, sans-serif;
   font-weight: 600;
   font-size: 14px;
   line-height: 1.42;
-  color: #FFFFFF;
+  color: #ffffff;
 `;
